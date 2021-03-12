f8e085d7f2808435d0c30f07ba95d0b8dace4709
Mikorolo <mikolaj.widanka@gmail.com>
2021-03-12 23:56:08 +0100

    Changed responsivnes function

--- app.js
+++ app.js
@@ -25,3 +25,46 @@ function takevalue()
 
     divResults.append(a, b, c, d, e);
 }
+
+/*=====================================================================*/
+
+function checkValue()
+{
+
+    event.preventDefault();
+    let firstNameValue=document.getElementById("firstName").value;
+    let surnameValue=document.getElementById("surname").value;
+    let telNumValue=document.getElementById("telNum").value;
+    let emailValue=document.getElementById("mail").value;
+    let messageValue=document.getElementById("message").value;
+
+    const divResults = document.getElementById("results");
+
+    let a = document.createElement("h4");
+    let b = document.createElement("h4");
+    let c = document.createElement("h4");
+    let d = document.createElement("h4");
+    let e = document.createElement("h4");
+
+    if(telNumValue.length != 9) {
+        alert('Numer powinien mieć 9 cyfr');
+
+        a.textContent = "Twoje imię: " + firstNameValue;
+        b.textContent = "Twoje nazwisko: " + surnameValue;
+        c.textContent = "Twój numer: Błędny numer";
+        d.textContent = "Twój email: " + emailValue;
+        e.textContent = "Twoja wiadomość: " + messageValue;
+
+        divResults.append(a, b, c, d, e);
+    }
+    else {
+
+        a.textContent = "Twoje imię: " + firstNameValue;
+        b.textContent = "Twoje nazwisko: " + surnameValue;
+        c.textContent = "Twój numer: " + telNumValue;
+        d.textContent = "Twój email: " + emailValue;
+        e.textContent = "Twoja wiadomość: " + messageValue;
+        
+        divResults.append(a, b, c, d, e);
+    }
+}
